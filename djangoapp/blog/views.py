f94bac02487b1679d8aab6cd5739598d29da255d
ferreiraryan <ryck342020@hotmail.com>
2025-11-22 18:00:45 -0300

    Filtrando: e ordenando os posts enviados para a view

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,22 +1,46 @@
+from blog.models import Post
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+PER_PAGE = 9
 
 
 def index(request):
+    posts = (
+        Post
+        .objects
+        .filter(is_published=True)
+        .order_by('-pk')
+    )
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
-        'blog/pages/index.html'
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
     )
 
 
 def page(request):
     return render(
         request,
-        'blog/pages/page.html'
+        'blog/pages/page.html',
+        {
+            # 'page_obj': page_obj,
+        }
     )
 
 
 def post(request):
     return render(
         request,
-        'blog/pages/post.html'
+        'blog/pages/post.html',
+        {
+            # 'page_obj': page_obj,
+        }
     )
