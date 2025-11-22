831a5508961ad2298f78b8abfb243eb4963871e5
ferreiraryan <ryck342020@hotmail.com>
2025-11-22 18:16:11 -0300

    feat: criando um campo readonly na admin do Django

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -6,12 +6,7 @@ PER_PAGE = 9
 
 
 def index(request):
-    posts = (
-        Post
-        .objects
-        .filter(is_published=True)
-        .order_by('-pk')
-    )
+    posts = Post.objects.get_published()
 
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
@@ -36,7 +31,7 @@ def page(request):
     )
 
 
-def post(request):
+def post(request, slug):
     return render(
         request,
         'blog/pages/post.html',
